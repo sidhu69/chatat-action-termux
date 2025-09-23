f5ea59ee19d4669fb84d55696c55b9fc8b264b2c
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2025-09-23 10:56:28 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.chatat.app',
   appName: 'Chatat',
   webDir: 'dist',
-  server: {
-    url: "https://53d8a5cb-a39c-40a4-a940-693e5d423c0b.lovableproject.com?forceHideBadge=true",
-    cleartext: true
-  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
