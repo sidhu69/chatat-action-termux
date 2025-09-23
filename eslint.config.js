8776f7844c7845de68fcab58cda939fa8eb3a45c
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2025-09-23 10:23:23 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -19,10 +19,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true, allowExportNames: ["default", "loader", "action", "meta"] }
-      ],
+      "react-refresh/only-export-components": "off",
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
